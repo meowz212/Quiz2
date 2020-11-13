040246eca049e74804c74affbff07561189996d8
meowz212 <kimoral21200@gmail.com>
2020-11-13 09:14:46 +0700

    initial commit

---
+++ Models/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarWeb.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public String Type { get; set; }
+        public String Name { get; set; }
+        public String Plate { get; set; }
+        public decimal Price { get; set; }
+        public String Description { get; set; }
+    }
+
+    public class CarDBContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
